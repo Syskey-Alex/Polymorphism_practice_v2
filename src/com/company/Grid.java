12554b677cb7ffc67e2a264c2225c86cafbfc008
Syskey-Alex <alexionita0207@yahoo.com>
2022-03-05 12:13:31 +0000

    Initial commit

---
+++ src/com/company/Grid.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Grid {
+  //  public int[][] myGrid= new myGrid [10][10];
+    public void myGrid() {
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 1; j <=10; j++) {
+                    System.out.print("▢");
+                }
+            //loop prints out the numbers 1-10
+            System.out.println();
+        }
+    }
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Grid myGrid = new Grid();
+        myGrid.myGrid();
+    }
+
+   // public int getValue(int col, int row) {
+    //    int column = col;
+     //   int row_ = row;
+    //}
+
+    //public int setValue(int col, int row) {
+     //   return col,row;
+}
